17fcabd89e20f26c3c5456143bdda35a2f473bf6
Sanjay Shukla <ss2271652@gmail.com>
2024-08-23 14:52:56 +0530

    Pinned Comment position Changes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,13 +7,15 @@ function Note({ title, content, onDelete, onPin, isPinned }) {
   return (
     <div className="note">
       <div className="note-data">
+        <div className="note-title">
         <h1>{title}</h1>
-        <p>{content}</p>
-      </div>
-      <div className="note-button">
         <button onClick={onPin}>
           <MdOutlinePushPin size={25} style={{ color: isPinned ? "gold" : "black" }} />
         </button>
+        </div>
+        <p>{content}</p>
+      </div>
+      <div className="note-button">
         <button onClick={onDelete}>
           <MdDelete size={25} />
         </button>
